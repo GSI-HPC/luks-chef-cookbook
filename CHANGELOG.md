cf61d04d0c894f1f2feb635774bffcb91f20e2fc
Christopher Huhn <C.Huhn@gsi.de>
2023-11-01 15:02:08 +0100

    Initial cookbook
    Cookbook created with `cinc generate cookbook -m "linuxgroup@gsi.de"
        --copyright "GSI Helmholtzzentrum für Schwerionenforschung GmbH"
        -I gplv3 -V luks`

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## Unreleased
+
+Cookbook created with
+```
+cinc generate cookbook -m "linuxgroup@gsi.de" \
+    --copyright "GSI Helmholtzzentrum für Schwerionenforschung GmbH" \
+    -I gplv3 -V luks
+```
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'luks'
+maintainer 'GSI Helmholtzzentrum für Schwerionenforschung GmbH'
+maintainer_email 'linuxgroup@gsi.de'
+license 'GPL-3.0'
+description 'Installs/Configures luks'
+version '0.1.0'
+chef_version '>= 16.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/luks/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/luks'
