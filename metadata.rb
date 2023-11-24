5e03adc2b03e727e7d97079423c3a249fbe7cb41
Christopher Huhn <C.Huhn@gsi.de>
2023-11-24 12:33:45 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -4,16 +4,7 @@ maintainer_email 'linuxgroup@gsi.de'
 license 'GPL-3.0'
 description 'Installs/Configures luks'
 version '0.1.0'
-chef_version '>= 16.0'
+chef_version '>= 12.5'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/luks/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/luks'
+source_url 'https://git.gsi.de/chef/cookbooks/luks'
+issues_url 'https://git.gsi.de/chef/cookbooks/luks/issues'
